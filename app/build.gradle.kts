8173908edb39bb11d2e6aa278667b70cd8d1fb73
Frostre_ <n9043395@gmail.com>
2026-09-14 06:48:29 +0200

    Change the VersionName to 0.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
         minSdk = 24
         targetSdk = 35
         versionCode = 1
-        versionName = "0.17"
+        versionName = "0.1"
 
         ndk {
             abiFilters += "arm64-v8a"
